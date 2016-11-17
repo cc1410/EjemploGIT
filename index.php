bdcc59bcc3a2bfac543ef48f526999a4ac682964
cc1410 <chen986755611@gmail.com>
2016-11-17 16:47:47 +0100

    formulario html creado

--- index.php
+++ index.php
@@ -5,8 +5,21 @@
         <title>Ejemplo de Formulario de Alta</title>
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <form action="datos.php" method="POST">
+            <div>nombre: <input type="text" name="nombre"></div>
+            <div>Edad: <input type="number" name="edad"></div>
+            <div>Ciudad: <select name="ciudad">
+                <option>Barcelona</option>
+                <option>Girona</option>
+                <option>Lleida</option>
+                <option>Tarragona</option>
+                </select></div>
+            <div>
+            sexo: 
+            Hombre<input type="radio" name="sexo" value="hombre">
+            Mujer<input type="radio" name="sexo" value="mujer">
+            </div>
+            <input type="submit" value="enviar">
+            </form>
     </body>
 </html>
